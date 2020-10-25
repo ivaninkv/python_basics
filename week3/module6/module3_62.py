84bbdf4d0667ca2416e3594538e7ec48f0b88f47
Konstantin Ivanin <ivanin.k@gmail.com>
2020-10-25 19:22:22 +0300

    module 3.6 completed

--- week3/module6/module3_62.py
+++ week3/module6/module3_62.py
@@ -3,19 +3,38 @@ import requests
 import json
 
 
+def read_ids(filename='dataset.txt'):
+    authors_ids = []
+    with open(filename) as f:
+        for line in f:
+            authors_ids.append(line.strip())
+    return authors_ids
+
+
+def artsy_request(author_id, token):
+    headers = {'X-Xapp-Token': token}
+    r = requests.get(f'https://api.artsy.net/api/artists/{author_id}',
+                     headers=headers)
+    return json.loads(r.text)
+
+
+def write_result(author_names, filename='authors.txt'):
+    with open(filename, 'w', encoding='UTF-8') as f:
+        f.writelines('\n'.join(author_names))
+        f.write('\n')
+
+
 def main():
+    authors_ids = read_ids()
     token = artsy.get_token()
-
-    # создаем заголовок, содержащий наш токен
-    headers = {"X-Xapp-Token": token}
-    # инициируем запрос с заголовком
-    r = requests.get("https://api.artsy.net/api/artists/4d8b92b34eb68a1b2c0003f4", headers=headers)
-
-    # разбираем ответ сервера
-    j = json.loads(r.text)
-
-    print(j)
+    author_names = []
+    for author_id in authors_ids:
+        j = artsy_request(author_id, token)
+        author_names.append((j['sortable_name'], int(j['birthday'])))
+    author_names.sort()  # сортировака по именам
+    author_names.sort(key=lambda x: x[1])  # сортировка по году рождения
+    write_result([author[0] for author in author_names])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
